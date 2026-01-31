0d5da74ab032d658f1b3afe8d6f8487184a1e013
mythilimarimganti <107639976+mythilimarimganti@users.noreply.github.com>
2026-01-31 22:27:46 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,11 @@
+##Basic Mathematical Operations
+first_num= int(input("Enter first number: "))
+second_num= int(input("Enter second number: "))
+add=first_num+second_num
+subtraction=first_num-second_num
+multiplication=first_num*second_num
+division=first_num/second_num
+print("The sum is:",add)
+print("The difference is:",subtraction)
+print("The multiplication is:",multiplication)
+print("The division is:",division)
---
+++ task2.py
@@ -0,0 +1,4 @@
+#Personalized Greeting
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+print("Helloo!!",first_name,last_name, "Welcome to the Python Programming!!!")
